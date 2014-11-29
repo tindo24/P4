eed9d2836ccadf8e2624e11cc51b6d13e7193beb
tindo24 <tindo24@gmail.com>
2014-11-29 16:01:12 -0500

    push files

---
+++ app/config/production/database.php
@@ -0,0 +1,18 @@
+
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'schooldata',
+            'username'  => 'root',
+            'password'  => 'wMNuQiuRzv',
+        ),
+
+    ),
+
+);
