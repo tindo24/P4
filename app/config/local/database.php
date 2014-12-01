4a49e9326a03c8aa568ae99db2700195d3c0b306
tindo24 <tindo24@gmail.com>
2014-12-01 15:01:41 -0500

    push files

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'schooldata',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'wMNuQiuRzv',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
